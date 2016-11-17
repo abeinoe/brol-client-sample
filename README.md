b654e04860aecb22a7b922c6730223ae3dc9dcc5
Abraham Inoerawan <abeinoe@gmail.com>
2016-11-17 11:36:42 +0700

    adding socket.io using nativescript-socketio plugin.

---
+++ README.md
@@ -0,0 +1,12 @@
+### Just Sample for Brol client
+
+#### initial
+
+```
+$ tns platform add android
+$ tns plugin add nativescript-socketio
+```
+
+#### run
+
+`$ tns run android`
--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,4 +1,6 @@
 var Observable = require("data/observable").Observable;
+var SocketIO = require('nativescript-socketio');
+const server = "chat.kodekreatif.co.id";
 
 function getMessage(counter) {
     if (counter <= 0) {
@@ -8,14 +10,32 @@ function getMessage(counter) {
     }
 }
 
+function getSocketMessage(isAuthenticated) {
+  if (isAuthenticated) {
+    return "connected to server";
+  } else {
+    return "connection to server denied";
+  }
+}
+
 function createViewModel() {
     var viewModel = new Observable();
     viewModel.counter = 42;
     viewModel.message = getMessage(viewModel.counter);
+    viewModel.sockmessage = "not connected";
+    viewModel.socketIO = new SocketIO( server, {});
+    viewModel.socketIO.on('authenticated', function() {
+      this.set("sockmessage", getSocketMessage(true));
+    });
+    viewModel.socketIO.on('unauthorized', function() {
+      this.set("sockmessage", getSocketMessage(false));
+    });
 
     viewModel.onTap = function() {
         this.counter--;
         this.set("message", getMessage(this.counter));
+        this.socketIO.connect();
+        this.socketIO.emit('authentication', {username: 'abe', password : 'eba'});
     }
 
     return viewModel;
